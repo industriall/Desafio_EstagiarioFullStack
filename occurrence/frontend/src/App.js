61d1031369c190779fb30fcb82f60388375793b8
cainajuca <cainajuca@gmail.com>
2021-03-17 12:33:58 -0300

    Estrutura inicial dos componentes de navegaçao e de formulario.

---
+++ occurrence/frontend/src/App.js
@@ -0,0 +1,17 @@
+import Nav from './components/Nav'
+import OccForm from './components/occForm'
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <Nav/>
+
+      <OccForm/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ occurrence/frontend/src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav>
+            <div className="nav-wrapper container">
+                <a href="/" className="brand-logo">Occurrence</a>
+                
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
+
+// deixa a barra transparente
+// .navbar-inner {
+//     background:transparent;
+// }
